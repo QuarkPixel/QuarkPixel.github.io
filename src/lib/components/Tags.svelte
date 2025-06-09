ec0e6902bcdaef25a1d551240e023afd613e3e61
QuarkPixel <m2462864403@gmail.com>
2025-06-09 21:10:13 +0800

    style: change font weight

--- src/lib/components/Tags.svelte
+++ src/lib/components/Tags.svelte
@@ -6,7 +6,7 @@
 	<div class="flex items-end gap-2">
 		{#each tags as tag, i (i)}
 			<div
-				class="group-hover/item:bg-secondary-100-900 duration-300 bg-secondary-50-950 text-secondary-950-50 text-sm font-caveat font-bold
+				class="group-hover/item:bg-secondary-100-900 duration-300 bg-secondary-50-950 text-secondary-950-50 text-sm font-caveat
 					rounded px-2 whitespace-nowrap"
 			>
 				{tag}
