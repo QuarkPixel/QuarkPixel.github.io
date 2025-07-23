2b995b57765078b1edc20c9fad552de06cb10f80
QuarkPixel <m2462864403@gmail.com>
2025-07-23 15:51:02 +0800

    fix(layout): AGAIN, I gave up. Fuck you Apple.

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,15 +1,7 @@
-// +layout.ts
 import { error } from '@sveltejs/kit';
-import { isSafari } from '$lib/utils/isSafari.js';
-import { browser } from '$app/environment';
-
 export const prerender = true;
 
 export const load = async ({ params, url }: { params: Record<string, string>; url: URL }) => {
-	// 判断是否为 Safari 浏览器
-	// src/app.css:25
-	if (browser && !isSafari()) document.documentElement.classList.add('not-safari');
-
 	// 根据路径返回不同的配置
 	switch (true) {
 		case url.pathname.startsWith('/p/'): {
