d3892d36e6799bf59c1940fbde7fd6099893f91b
QuarkPixel <m2462864403@gmail.com>
2025-07-23 16:23:39 +0800

    format

--- src/lib/utils/isFirefox.ts
+++ src/lib/utils/isFirefox.ts
@@ -1,5 +1,4 @@
 export function isFirefox() {
 	const userAgent = navigator.userAgent.toLowerCase();
-	console.log(userAgent, userAgent.includes('firefox'))
 	return userAgent.includes('firefox');
 }
