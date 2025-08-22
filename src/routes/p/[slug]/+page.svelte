d44ca0928a20efeee44473eff82c24e10b8e2cfa
QuarkPixel <m2462864403@gmail.com>
2025-08-22 19:16:03 +0800

    fix: og:description

--- src/routes/p/[slug]/+page.svelte
+++ src/routes/p/[slug]/+page.svelte
@@ -11,9 +11,9 @@
 	<meta name="author" content={data.metadata.author} />
 	<meta name="keywords" content={data.metadata.tags.join(', ')} />
 	<meta property="og:title" content="{data.title} - Hsuan's Blog" />
-	<meta property="og:description" content={data.metadata.description} />
+	<meta property="og:description" content={data.metadata.description.join('')} />
 	<meta property="og:type" content="article" />
 	<meta property="article:author" content={data.metadata.author} />
-	<meta property="article:published_time" content={data.metadata.date as string} />
+	<meta property="article:published_time" content={data.metadata.date.toISOString()} />
 	<meta property="article:tag" content={data.metadata.tags.join(', ')} />
 </svelte:head>
