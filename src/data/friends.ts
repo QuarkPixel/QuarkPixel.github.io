0b0d17d565047d79df89e7b3937d18db817338b8
QuarkPixel <m2462864403@gmail.com>
2025-08-23 13:15:36 +0800

    add link

--- src/data/friends.ts
+++ src/data/friends.ts
@@ -7,20 +7,29 @@ export interface FriendLink {
 
 export const friendLinks: FriendLink[] = [
 	{
-		"href": "https://studiountagged.top/",
-		"name": "Studio Untagged",
-		"desc": "字体排印主题相关博客",
-		"icon": "tabler:typography"
-	}, {
-		"href": "https://blog.gxres.net/",
-		"name": "Restent's Notebook"
-	}, {
-		"href": "https://play.mcpark.cc/",
-		"name": "McPark",
-		"desc": "找服玩就上McPark",
-		"icon": "tabler:device-gamepad"
-	}, {
-		"href": "https://blog.imlcd.cn/link.html",
-		"name": "忆云小站",
+		href: 'https://studiountagged.top/',
+		name: 'Studio Untagged',
+		desc: '字体排印主题相关博客',
+		icon: 'tabler:typography'
+	},
+	{
+		href: 'https://www.geedea.pro/',
+		name: '極客死亡計劃',
+		desc: '我平时很喜欢阅读的博客',
+		icon: 'tabler:skull'
+	},
+	{
+		href: 'https://blog.gxres.net/',
+		name: "Restent's Notebook"
+	},
+	{
+		href: 'https://play.mcpark.cc/',
+		name: 'McPark',
+		desc: '找服玩就上McPark',
+		icon: 'tabler:device-gamepad'
+	},
+	{
+		href: 'https://blog.imlcd.cn/link.html',
+		name: '忆云小站'
 	}
-]
+];
