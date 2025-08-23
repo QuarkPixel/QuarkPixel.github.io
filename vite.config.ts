f298aa2e9ce595603b3ecb57d879a03f2ec3254a
QuarkPixel <m2462864403@gmail.com>
2025-08-23 21:29:52 +0800

    add alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import { generatePostsList, giscusThemePlugin, rssPlugin } from './tools/vite/index.js';
 
 export default defineConfig({
@@ -11,6 +12,11 @@ export default defineConfig({
     giscusThemePlugin(),
     rssPlugin()
   ],
+  resolve: {
+    alias: {
+      '$posts': resolve('./posts')
+    }
+  },
   server: {
     fs: {
       allow: ['posts']
