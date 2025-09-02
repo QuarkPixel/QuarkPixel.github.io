7e579f535d774628ae70be0d86b38247d4f440f9
QuarkPixel <m2462864403@gmail.com>
2025-09-02 16:54:34 +0800

    fix

--- src/lib/layouts/blog.svelte
+++ src/lib/layouts/blog.svelte
@@ -1,3 +1,10 @@
+<script lang="ts" module>
+	/* eslint-disable no-import-assign */
+	import Blockquote from '$lib/components/typography/Blockquote.svelte';
+
+	export { Blockquote as blockquote };
+</script>
+
 <script lang="ts">
 	import '$lib/styles/code-highlight.css';
 	import type { Metadata } from '$lib/types/postData.js';
@@ -6,20 +13,12 @@
 	import Comment from '$lib/components/Comment.svelte';
 	import BudouX from '$lib/components/BudouX.svelte';
 
-
 	interface Props {
 		children: Snippet;
 		metadata: Metadata;
 	}
 
 	const { children, metadata }: Props = $props();
-</script>
-
-<script lang="ts" module>
-	/* eslint-disable no-import-assign */
-	import Blockquote from '$lib/components/typography/Blockquote.svelte';
-
-	export { Blockquote as blockquote };
 </script>
 
 <link
@@ -30,7 +29,7 @@
 />
 
 <article class="prose lg:prose-xl my-4 w-[80%]">
-	<header class="mb-10">
+   	<header class="mb-10">
 		<h1 class="mb-2">
 			<BudouX text={metadata.title} />
 		</h1>
@@ -61,81 +60,79 @@
 </article>
 
 <style lang="scss" global>
-  .prose {
-    word-break: auto-phrase;
+	.pro { word-break: auto-phrase;
+		code:not(pre code) {
+			font-size: smaller;
+		}
 
-    code:not(pre code) {
-      font-size: smaller;
-    }
+		& > details > summary {
+			font-size: smaller;
+			opacity: 0.8;
+		}
 
-    & > details > summary {
-      font-size: smaller;
-      opacity: 0.8;
-    }
+		:where(a):not(:where([class~='not-prose'], [class~='not-prose'] *)) {
+			text-decoration: none;
+			position: relative;
+			color: var(--tw-prose-body);
+			pointer-events: none;
 
-    :where(a):not(:where([class~="not-prose"],[class~="not-prose"] *)) {
-      text-decoration: none;
-      position: relative;
-      color: var(--tw-prose-body);
+			$scale: 0.075;
+			$width: 8em * $scale;
+			$height: 5em * $scale;
+			$neg-width: -8em * $scale;
+			$bottom: -5em * $scale;
 
-      $scale: .075;
-      $width: 8em * $scale;
-      $height: 5em * $scale;
-      $neg-width: -8em * $scale;
-      $bottom: -5em * $scale;
+			&::after {
+				content: '';
+				position: absolute;
+				left: 0;
+				bottom: $bottom;
+				width: 100%;
+				height: $height;
+				mask-image: url('/assets/wave.svg');
+				mask-repeat: repeat-x;
+				mask-size: $width $height;
+				background-color: color-mix(in oklab, var(--tw-prose-body) 30%, transparent);
+				transition: background-color 0.1s ease-in-out;
+				animation: wavy 0.3s linear infinite;
+				animation-play-state: paused; /* 默认暂停 */
+			}
 
-      &::after {
-        content: '';
-				font-size: inherit;
-        position: absolute;
-        left: 0;
-        bottom: $bottom;
-        width: 100%;
-        height: $height;
-        mask-image: url('/assets/wave.svg');
-        mask-repeat: repeat-x;
-        mask-size: $width $height;
-        background-color: color-mix(in oklab, var(--tw-prose-body) 30%, transparent);
-        transition: background-color .1s ease-in-out;
-        animation: wavy .3s linear infinite;
-        animation-play-state: paused; /* 默认暂停 */
-      }
+			&:hover::after {
+				background-color: var(--color-secondary-500);
+				animation-play-state: running; /* 悬停时运行动画 */
+			}
 
-      &:hover::after {
-        background-color: var(--color-secondary-500);
-        animation-play-state: running; /* 悬停时运行动画 */
-      }
+			@keyframes wavy {
+				0% {
+					mask-position: 0 0;
+				}
+				100% {
+					mask-position: $neg-width 0; /* 与 background-size 的宽度一致 */
+				}
+			}
+		}
 
-      @keyframes wavy {
-        0% {
-          mask-position: 0 0;
-        }
-        100% {
-          mask-position: $neg-width 0; /* 与 background-size 的宽度一致 */
-        }
-      }
-    }
+		.anchor-link {
+			font: inherit;
+			color: inherit;
+			text-decoration: inherit;
+			position: relative;
 
-    .anchor-link {
-      font: inherit;
-      color: inherit;
-      text-decoration: inherit;
-      position: relative;
+			&::before {
+				content: '#';
+				position: absolute;
+				left: -1em;
+				font-family: var(--font-caveat);
+				opacity: 0;
+				transition: 0.2s;
+				padding-right: 1em;
+				pointer-events: none;
+			}
 
-      &::before {
-        content: '#';
-        position: absolute;
-        left: -1em;
-        font-family: var(--font-caveat);
-        opacity: 0;
-        transition: 0.2s;
-        padding-right: 1em;
-        pointer-events: none;
-      }
-
-      &:hover::before {
-        opacity: 0.4;
-      }
-    }
-  }
+			&:hover::before {
+				opacity: 0.4;
+			}
+		}
+	}
 </style>
