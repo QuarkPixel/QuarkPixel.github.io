b05fb3ca29426ef45e53cac175610a74de7ff53a
QuarkPixel <m2462864403@gmail.com>
2025-09-04 15:21:16 +0800

    feat(tags): Tags now avaiable to custom color

--- src/lib/components/Tags.svelte
+++ src/lib/components/Tags.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	const { tags }: { tags: string[] } = $props();
+	const { tags, color = 'bg-secondary-500/40' }: { tags: string[], color?: string } = $props();
 </script>
 
 {#if tags.length > 0}
 	<div class="flex items-center gap-2">
 		{#each tags as tag, i (i)}
 			<div
-				class="bg-secondary-500/40 text-secondary-950-50 text-sm font-caveat rounded px-2 whitespace-nowrap"
+				class="{color} text-secondary-950-50 text-sm font-caveat rounded px-2 whitespace-nowrap"
 			>
 				{tag}
 			</div>
